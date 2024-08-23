9c40a80fdcb019f02db57447a19b99ebe0acff07
Andik Kurniawan <andikk362@gmail.com>
2024-08-23 15:58:49 +0700

    Prepare Ledger

---
+++ database/migrations/2024_08_23_155750_create_ledgers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ledgers', function (Blueprint $table) {
+            $table->id();
+            $table->string('journal_id');
+            $table->foreign('journal_id')->references('id')->on('journals');
+            $table->string('account_id');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->text('description');
+            $table->double('debit');
+            $table->double('credit');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ledgers');
+    }
+};
